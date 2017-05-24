785a96fea699ca58385a8ef56cf84fa504221790
Qi Le <qile@onest.net>
2017-05-24 13:43:09 +0800

    练习1-3

---
+++ 01.引擎简介及环境搭建/练习/LS01/Assets/LS01_Sphere_Test.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LS01_Sphere_Test : MonoBehaviour {
+	GameObject target;
+	// Use this for initialization
+	void Start () {
+		target = GameObject.Find ("Cube");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.RotateAround (target.transform.position, Vector3.up, 5);
+	}
+}
